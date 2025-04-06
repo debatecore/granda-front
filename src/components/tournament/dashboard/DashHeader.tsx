d3f2546af506b2a12a1fb3652f9db97e13003172
jakubmanczak <jakub@manczak.net>
2025-04-06 10:32:39 +0200

    mobile friendly dashboard

--- src/components/tournament/dashboard/DashHeader.tsx
+++ src/components/tournament/dashboard/DashHeader.tsx
@@ -11,21 +11,25 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import Link from "next/link";
 import { LogOutDashHeaderDropdownButton } from "./Logout";
 
 const DashHeader = ({ t, u }: { t: Tournament; u: User }) => {
   return (
     <>
       <div className="space-x-1 z-40">
-        <span className="font-semibold font-logo z-40">{t.full_name}</span>
-        <span className="text-xs font-light z-40">{`(${t.shortened_name})`}</span>
+        <span className="font-semibold font-logo z-40 hidden xs:inline">
+          {t.full_name}
+        </span>
+        <span className="text-xs font-light z-40 hidden xs:inline">{`(${t.shortened_name})`}</span>
+        <span className="font-semibold font-logo z-40 xs:hidden">
+          {t.shortened_name}
+        </span>
         {/* this should be the separator between breadcrumb steps.. */}
         {/* <LucideChevronRight className="inline" size={16} /> */}
       </div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <div className="ml-auto flex gap-4 px-3 py-1 bg-stone-700/40 border border-stone-700 rounded z-40 cursor-pointer">
+          <div className="ml-auto flex gap-4 px-3 py-1 bg-stone-700/40 hover:bg-stone-600/40 focus:bg-stone-600/40 border border-stone-700 hover:border-stone-500 focus:border-stone-500 rounded z-40 cursor-pointer">
             <p className="font-semibold tracking-tight">{u.handle}</p>
             <LucideUserCircle />
           </div>
--- src/components/tournament/dashboard/Dashboard.tsx
+++ src/components/tournament/dashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-// import { useState } from "react";
+import { LucideSidebar, LucideX } from "lucide-react";
+
+import { useState } from "react";
 
 const Dashboard = ({
   sidebar,
@@ -11,9 +13,9 @@ const Dashboard = ({
   header: React.ReactNode;
   children: React.ReactNode;
 }) => {
-  // const [showSide, setShowSide] = useState<boolean>(true);
+  const [showSide, setShowSide] = useState<boolean>(true);
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen relative">
       {/* DESKTOP SIDEBAR */}
       <div className="hidden lg:block border-r border-stone-700 bg-stone-900">
         {sidebar}
@@ -21,11 +23,28 @@ const Dashboard = ({
       </div>
       {/* MOBILE SIDEBAR */}
       <div className="block lg:hidden">
-        {/*  */}
-        {/*  */}
+        {showSide && (
+          <>
+            <div className="absolute h-full w-full xs:w-72 left-0 top-0 bg-stone-900 z-150 border-r border-stone-700">
+              {sidebar}
+              <button
+                className="absolute top-7 right-6 cursor-pointer rounded border border-stone-700 hover:border-stone-500 focus:border-stone-500 bg-stone-700/40 hover:bg-stone-600/40 focus:bg-stone-600/40 z-160"
+                onClick={() => setShowSide(false)}
+              >
+                <LucideX />
+              </button>
+            </div>
+          </>
+        )}
       </div>
       <div className="flex flex-1 flex-col">
         <header className="relative flex max-h-20 min-h-20 h-full items-center bg-stone-900 gap-4 border-b border-stone-700 px-4 md:px-6 overflow-hidden">
+          <button
+            className="block lg:hidden p-1 rounded border border-stone-700 hover:border-stone-500 focus:border-stone-500 bg-stone-700/40 hover:bg-stone-600/40 focus:bg-stone-600/40 text-stone-100 z-40 cursor-pointer"
+            onClick={() => setShowSide(true)}
+          >
+            <LucideSidebar />
+          </button>
           {header}
         </header>
         <div className="block w-full h-full overflow-scroll">{children}</div>
