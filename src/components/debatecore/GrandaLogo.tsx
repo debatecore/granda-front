a12a1f9e8a02143827b141284d9a8cc4b51d4184
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2025-09-28 10:44:51 +0200

    refactor: improve polish translations per code review suggestions

--- src/components/debatecore/GrandaLogo.tsx
+++ src/components/debatecore/GrandaLogo.tsx
@@ -1,5 +1,3 @@
-import { useTranslations } from "next-intl";
-
 const GrandaLogo = ({
   className: grandaClassName,
   subtitle = true,
@@ -7,13 +5,12 @@ const GrandaLogo = ({
   className: string;
   subtitle?: boolean;
 }) => {
-  const t = useTranslations("debatecore");
   return (
     <div className="select-none">
       <h1 className={`${grandaClassName} text-center font-logo`}>{"granda"}</h1>
       {subtitle ? (
         <p className="font-fancy text-stone-400 -mt-1 ml-15 text-center">
-          {t("logo_subtitle")}
+          {"The Debate Tournament Planner"}
         </p>
       ) : (
         ""
