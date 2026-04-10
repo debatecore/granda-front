c4c9c523960b2611fc21d6a5721987b8d7f554fe
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2026-04-10 08:42:14 +0200

    refactor: provide default env variables in compose.yaml

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,17 +7,15 @@ export function cn(...inputs: ClassValue[]) {
 
 // TO-DO: figure out how to pass env variables to the container
 // (it doesn't seem to work)
-const INTERNAL_BACKEND_SOCKET = process.env.INTERNAL_BACKEND_SOCKET
-  ? process.env.INTERNAL_BACKEND_SOCKET
-  : "http://server-prod:2023";
-const BACKEND_SOCKET = process.env.BACKEND_SOCKET
-  ? process.env.BACKEND_SOCKET
-  : "http://localhost:2023";
+const INTERNAL_BACKEND_SOCKET =
+  process.env.INTERNAL_BACKEND_SOCKET || "http://server-prod:2023";
+const BACKEND_SOCKET = process.env.BACKEND_SOCKET || "http://localhost:2023";
 
 export const fetchClientSide = async (
   path: string | URL | globalThis.Request,
   init?: RequestInit,
 ) => {
+  console.log(process.env);
   console.log(BACKEND_SOCKET);
   return fetch(`${BACKEND_SOCKET}${path}`, {
     credentials: "include",
