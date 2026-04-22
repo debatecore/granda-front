e9f858b58a146d59e3aec3642aec9ba6c53bc578
Peres270 <470.eitv@gmail.com>
2026-04-22 15:46:27 +0200

    fix: remove unrelated ladder translations and adjust Debate type

--- src/types/Debate.ts
+++ src/types/Debate.ts
@@ -1,7 +1,7 @@
 type Debate = {
-  id?: string;
-  marshal_user_id: string;
-  motion_id: string;
+  id: string;
+  marshal_user_id?: string;
+  motion_id?: string;
   round_id: string;
   tournament_id: string;
 };
