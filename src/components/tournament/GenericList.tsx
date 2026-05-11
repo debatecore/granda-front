22683761ecad203f52be6f7ca80bb6d47d0fb7d2
Madoka Matsuzaki <m98763802@gmail.com>
2026-05-11 19:52:44 +0200

    Refactor DebatesList UI to match the new design

--- src/components/tournament/GenericList.tsx
+++ src/components/tournament/GenericList.tsx
@@ -1,7 +1,25 @@
 import { ReactNode } from "react";
+import { LucideIcon } from "lucide-react";
 
-const GenericList = ({ children }: { children: ReactNode }) => {
-  return <div className="flex w-full flex-col gap-4">{children}</div>;
+type GenericListProps = {
+  children: ReactNode;
+  icon?: LucideIcon;
+};
+
+const GenericList = ({ children, icon: ICON }: GenericListProps) => {
+  return (
+    <div className="flex w-full flex-col gap-6">
+      <div className="flex w-full flex-col gap-3">
+        {ICON && (
+          <div className="flex h-8 w-8 items-center justify-center rounded bg-pink-500 text-white">
+            <ICON size={20} />
+          </div>
+        )}
+      </div>
+
+      <div className="flex w-full flex-col gap-4">{children}</div>
+    </div>
+  );
 };
 
 export { GenericList };
