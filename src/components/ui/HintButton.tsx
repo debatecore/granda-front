a106363239ba047e10a861aad88de298ce3be2b6
nadia <nadiakingan@gmail.com>
2026-05-25 23:01:47 +0200

    fix questionicon, remove ondismiss temporarily

---
+++ src/components/ui/HintButton.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { IconQuestion } from "../icons/QuestionIcon";
+import { useState } from "react";
+import { HintTextBox } from "./HintTextBox";
+
+interface HintButtonProps {
+  title: string;
+  content: string;
+  pointerDirection: "top" | "bottom" | "left" | "right";
+}
+
+const HintButton = ({ title, content, pointerDirection }: HintButtonProps) => {
+  const [showTutorial, setShowTutorial] = useState(false);
+
+  const textBoxPlacement = {
+    top: "bottom-full left-1/2 -translate-x-1/2 mb-[10px]",
+    bottom: "top-full left-1/2 -translate-x-1/2 mt-[10px]",
+    left: "right-full top-1/2 -translate-y-1/2 mr-[10px]",
+    right: "left-full top-1/2 -translate-y-1/2 ml-[10px]",
+  };
+  return (
+    <div className="relative inline-block">
+      <button
+        className="flex items-center justify-center p-[8px] bg-transparent hover:cursor-pointer"
+        onClick={() => setShowTutorial(!showTutorial)}
+      >
+        <IconQuestion />
+      </button>
+
+      <div
+        className={`absolute w-max z-50 ${textBoxPlacement[pointerDirection]}`}
+      >
+        <HintTextBox
+          title={title}
+          content={content}
+          visible={showTutorial}
+          pointerDirection={pointerDirection}
+          onDismiss={() => setShowTutorial(false)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export { HintButton };
--- src/components/ui/HintTextBox.tsx
+++ src/components/ui/HintTextBox.tsx
@@ -1,24 +1,39 @@
-type HintTextBoxProps = {
+interface HintTextBoxProps {
   title: string;
   content: string;
-  onClose: () => void;
-};
+  pointerDirection?: "top" | "bottom" | "left" | "right";
+  visible: boolean;
+  onDismiss: () => void;
+}
 
-const HintTextBox = (props: HintTextBoxProps) => {
+const HintTextBox = ({
+  title,
+  content,
+  pointerDirection = "bottom",
+  visible,
+}: HintTextBoxProps) => {
+  if (!visible) return null;
+
+  const arrowPosition = {
+    top: "after:top-full after:border-t-[10px] after:border-x-[12px] after:left-1/2 after:-ml-[12px] after:border-t-[#ffffff] mb-[8px]",
+    bottom:
+      "after:bottom-full after:border-b-[10px] after:border-x-[12px] after:left-1/2 after:-ml-[12px] after:border-b-[#ffffff] mt-[8px]",
+    left: "after:left-full after:border-l-[10px] after:border-y-[12px] after:top-1/2 after:-mt-[12px] after:border-l-[#ffffff] mr-[8px]",
+    right:
+      "after:right-full after:border-r-[10px] after:border-y-[12px] after:top-1/2 after:-mt-[12px] after:border-r-[#ffffff] ml-[8px]",
+  };
+
   return (
     <div
-      className="
-        relative inline-block flex-wrap align-top text-justify bg-[#ffffff] leading-[18px] min-w-[280px] max-w-[320px] p-[20px] mb-[16px] rounded-lg overflow-wrap
+      className={`
+        relative inline-block flex-wrap align-top text-justify bg-[#ffffff] leading-[16px] min-w-[330px] max-w-[3400px] py-[18px] px-[24px] rounded-lg
         
-        after:content-[''] after:absolute after:top-full after:left-1/2 after:-ml-[1em] after:w-0 after:h-0 after:border-t-[10px] after:border-x-[20px] after:-ml-[20px] after:border-transparent after:border-t-[#ffffff]"
+        after:content-[''] after:absolute after:w-0 after:h-0 after:border-transparent ${arrowPosition[pointerDirection]}`}
     >
-      <div className="text-[#333] text-[16px] font-bold pb-[10px]">
-        {props.title}
-      </div>
-      <div className="text-[#333] text-[12px]">{props.content}</div>
-      <div className="text-[#333] text-[12px] underline pt-[10px] text-right hover:cursor-pointer">
-        {" "}
-        OK{" "}
+      <div className="text-[#333] text-[16px] font-bold pb-[10px]">{title}</div>
+      <div className="text-[#333] text-[12px]">{content}</div>
+      <div className="text-[#333] text-[12px] underline pt-[6px] text-right hover:cursor-pointer">
+        OK
       </div>
     </div>
   );
